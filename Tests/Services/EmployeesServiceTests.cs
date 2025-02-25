a38c0499ac2ae53aee0dabf735f1e3272db96dfc
victtor01 <josevictot.ar@gmail.com>
2025-02-24 23:48:50 -0300

    test: Add tests of employee

--- Tests/Services/EmployeesServiceTests.cs
+++ Tests/Services/EmployeesServiceTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using PointSaleApi.Src.Core.Application.Interfaces;
 using PointSaleApi.Src.Core.Application.Records;
 using PointSaleApi.Src.Core.Application.Services;
+using PointSaleApi.Src.Core.Domain;
 using PointSaleApi.Src.Infra.Config;
 
 namespace Tests.Services;
@@ -24,9 +25,46 @@ public class EmployeesServiceTests
   {
     var createEmployeeDTO = new CreateEmployeeDTO(Salary: 1000 * 1000);
 
+    _employeeRepository.Setup(repo => repo.GetAllByManagerAndStoreAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+      .ReturnsAsync([]);
+
     var exception = await Assert.ThrowsExceptionAsync<BadRequestException>(
       () => _employeesService.CreateAsync(createEmployeeDTO, Guid.NewGuid(), Guid.NewGuid()));
 
     exception.Should().NotBeNull();
+  }
+
+  [TestMethod]
+  public async Task ItShouldReturnErrorBecauseCountOfEmployeeIsInvalid()
+  {
+    const int VALID_COUNT_OF_EMPLOYEE = 10;
+    const int INVALID_COUNT = 10 + VALID_COUNT_OF_EMPLOYEE;
+
+    Guid managerId = Guid.NewGuid();
+    Guid storeId = Guid.NewGuid();
+
+    List<Employee> employees = new List<Employee>();
+    for (int i = 0; i < INVALID_COUNT; i++)
+    {
+      employees.Add(new Employee
+      {
+        ManagerId = managerId,
+        StoreId = storeId,
+        Salary = 1000,
+      });
+    }
+
+    _employeeRepository.Setup(repo => repo
+        .GetAllByManagerAndStoreAsync(managerId, storeId))
+      .ReturnsAsync(employees);
+
+    decimal salaryOfEmployee = 900;
+    BadRequestException exception = await Assert.ThrowsExceptionAsync<BadRequestException>(
+      () => _employeesService.CreateAsync(new CreateEmployeeDTO(Salary: salaryOfEmployee), managerId, storeId));
+    
+    _employeeRepository.Verify(repo => repo.AddAsync(It.IsAny<Employee>()), Times.Never);
+    
+    exception.Should().NotBeNull();
+    exception.Message.Should().Contain("count of employees in database exceeded");
   }
 }
